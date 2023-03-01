bfeff522288cbddaffb4d6b7ce4605430b0543a6
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-01 13:11:59 +0000

    define a default head section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            title: 'Nuxt Store',
+            meta: [
+                { name: 'description', content: 'The greatest store you have ever seen.'}
+            ],
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+            ]
+        }
+    }
 })
